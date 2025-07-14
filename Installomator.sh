2c8b38a7fde759449620e3426cb334b358972df3
zachdekoning <zachdekoning@users.noreply.github.com>
2025-07-14 23:42:15 +0000

    Runner compiled Installomator.sh and Labels.txt using assemble.sh

--- Installomator.sh
+++ Installomator.sh
@@ -349,7 +349,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.9beta"
-VERSIONDATE="2025-07-01"
+VERSIONDATE="2025-07-14"
 
 # MARK: Functions
 
@@ -1566,9 +1566,9 @@ valuesfromarguments)
     type="pkg"
     packageID="com.1password.1password"
     downloadURL="https://downloads.1password.com/mac/1Password.pkg"
-    appNewVersion=$(curl -s https://releases.1password.com/mac/ | grep "1Password for Mac" | head -n 1 | grep -o ">Updated to.*on<" | grep -oE "[0-9].*[0-9]" | cut -d - -f 1)
+    appNewVersion=$(curl -s https://releases.1password.com/mac/index.xml | grep "1Password for Mac" | grep -v 'beta' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | tail -n 1)
     expectedTeamID="2BUA8C4S2C"
-    blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
+    blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" "1Password Helper" "1Password-BrowserSupport" )
     #forcefulQuit=YES
     ;;
 1passwordcli)
