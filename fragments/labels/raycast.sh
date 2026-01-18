ff715fac73bc04fff65909976c27164a11c80ddd
Zach de Koning <1870988+zachdekoning@users.noreply.github.com>
2026-01-19 09:20:00 +1100

    Fix raycast version detection

--- fragments/labels/raycast.sh
+++ fragments/labels/raycast.sh
@@ -2,6 +2,6 @@ raycast)
     name="Raycast"
     type="dmg"
     downloadURL="https://www.raycast.com/download"
-    appNewVersion="$( curl -fsIL "https://www.raycast.com/download" | grep -i ^location | grep Raycast_ | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/' )"
+    appNewVersion="$( echo $( curl -fsIL "https://www.raycast.com/download" | grep -i ^location | grep Raycast_ | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/' ) | cut -d ' ' -f 1 )"
     expectedTeamID="SY64MV22J9"
     ;;
